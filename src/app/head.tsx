bc65aae2b94029fab0fa0c2d3a2aaf1f77f3e50e
“[Muhammad <[muhammadahmed7924@gmail.com]>
2023-04-01 10:02:11 -0700

    my first commit

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+      <>
+        <title>Panaverse</title>
+        <link rel="icon" type="image/x-icon" href="/image/favicon.png"></link>
+
+      </>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,194 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import React from 'react'
+import head from './head'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+    <div className=' relative h-[3050px] w-full bg-gradient-to-r  from-slate-500 to-red-500 via-slate-300 '>
+      <div className=' relative  h-12 w-16 ml-1 pt-3'>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className=''>
+        <img  src='/image/6.png' alt='Panaverse logo'></img>
+        </div>
+        </div>
+        <div   className=' flex relative left-[420px]  text-2xl mt-[-40px] w-full'>
+        <div className=''>
+          <a href='/' className=' ml-5  hover:text-blue-600'>Home</a>
+          <a href='#about' className=' ml-5  hover:text-blue-600'>About</a>
+          <a href='#academic' className=' ml-5  hover:text-blue-600'>Academic</a>
+          <a className=' ml-5  hover:text-blue-600'>Apply</a>
+          <a href='#contact' className=' ml-5  hover:text-blue-600'>Contact</a>
+        </div>
         </div>
       </div>
+      <div className='flex justify-center  mt-[50px] h-80 w-full '>
+        <img src='/image/1.png'></img>
+      </div>
+      <div className=' absolute mt-[30px]'>
+        <h1 className=' text-center font-bold text-slate-900 text-5xl'>
+        Certified Web 3.0 and Metaverse Developer: A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar
+        </h1>
+      </div>
+<div className=' absolute mt-[300px]'>
+  <h1 id='about' className=' text-center font-bold text-slate-900 text-4xl'>
+  The Future of the Web is Web 3.0, Metaverse, and Edge Computing. Panaverse DAO is a movement to spread these technolgies globally. It is community of Web 3 and Metaverse developers, designers, trainers, startup founders and service providers.
+  </h1>
+  <h2 className=' font-bold text-slate-900 text-3xl mt-8 p-3'>
+  Program of Studies:
+  </h2>
+  <p className='flex justify-center font-semibold text-slate-900 text-xl p-3'>
+  This curriculum is intended for beginners who want to learn software development from the ground up. The first two quarters are shared by all specialities and are dedicated to studying Object-Oriented Programming and cutting-edge Full-Stack Web 2.0 development. It is going to be a year-long hybrid programme that includes both onsite and online classes and is divided into four quarters of 13 weeks each. The emphasis will be on hands-on learning by educating students to produce projects. To accommodate everyone, courses will be held primarily on weekends or after 6:00 p.m. (Pakistan Time) on weekdays. It employs a hybrid teaching format, with core onsite classes complemented by online Zoom laboratories and recorded videos.
+  </p>
+  <h2 className=' font-bold text-slate-900 text-3xl  p-3'>
+  Core Courses (Common in All Specializations)
+  </h2>
+  <p className=' flex justify-center font-semibold text-slate-900 text-xl p-3'>
+  Every participant of the program will start by completing the following two core courses:<br></br>
+Quarter I (Core) CS-101: Object-Oriented Programming using TypeScript<br></br>
+Quarter II (Core) W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform
+  </p>
+  <h2 className=' font-bold text-slate-900 text-3xl p-3'>
+  Specialized Tracks
+  </h2>
+  <p className=' flex justify-center font-semibold text-slate-900 text-xl p-3'>
+  After completing the first two quarters the participants will select one or more specializations consisting of two courses each:
+  </p>
+  <p className='flex justify-start font-bold text-slate-900 text-2xl p-3'>
+  1.Web 3.0 (Blockchain) and Metaverse Specialization<br></br>
+  2.Artificial Intelligence (AI) and Deep Learning Specialization<br></br>
+  3.Cloud-Native Computing Specialization<br></br>
+  4.Ambient Computing and IoT Specialization
+  </p>
+</div>
+      <div id='academic' className=' flex  justify-center mt-[1500px] w-full '>
+        <div className='service w-96 h-96 items-center max-w-xs mx-5 py-10 bg-black rounded-3xl shadow-black text-center p-8'>
+          <span className=' inline-block p-4 rounded-full bg-white text-white'>
+            <img className=' w-10 h-10' src='/image/2.png' alt='TypeScripy logo'></img>
+          </span>
+<h1 className=' mt-4 font-bold text-white text-2xl'>Quarter I (Core)</h1>
+<p className=' text-white mt-4'>
+CS-101: Object-Oriented Programming using TypeScript 
+Duration: 13 Weeks
+</p>
+</div>
+<div className='service w-96 h-96 items-center  max-w-xs mx-5 py-10 bg-black rounded-3xl shadow-md text-center p-8 '>
+          <span className=' inline-block p-4 rounded-full bg-white text-white' >
+            <img className=' w-10 h-10' src='/image/3.png' alt='Next.js 13 logo'></img>
+          </span>
+          <h1 className=' mt-4 font-bold text-white text-2xl' >Quarter II (Core)</h1>
+<p className='text-white mt-4'>W2-201: Developing Planet-Scale Web 2.0 Serverless <br></br>
+Duration: 13 Weeks
+</p>
+          </div>
+          <div className='service w-96 h-96 items-center max-w-xs mx-5 py-10 bg-black rounded-3xl shadow-md text-center p-8 '>
+          <span className=' inline-block p-4 rounded-full bg-white text-white' >
+            <img className=' w-10 h-10' src='/image/4.png' alt='Ethereum logo'></img>
+          </span>
+          <h1 className=' mt-4 text-white font-bold text-2xl' >Quarter III Web3 and Metaverse</h1>
+          <p className='text-white mt-4' >W3-351: Developing Web 3.0 Dapps<br></br>
+Duration: 13 Weeks
+</p>
+          </div>
+          <div className='service w-96 h-96 items-center max-w-xs mx-5 py-10 bg-black rounded-3xl shadow-md text-center p-8 '>
+          <span className=' inline-block p-4 rounded-full bg-white text-white' >
+            <img className=' w-10 h-10' src='/image/5.png' alt='Web 3.js logo' ></img>
+          </span>
+          <h1 className=' mt-4 text-white  font-bold text-2xl' >Quarter IV Web3 and Metaverse</h1>
+          <p className='text-white mt-4'>
+          MV-361: Developing Metaverse<br></br>
+Duration: 13 Weeks
+          </p>
+      </div>
+      </div>
+        <div className=" flex justify-center px-8 md:px-16  w-full min-h-screen" >
+      <div className=" py-8">
+        <a href="#">
+          <img src="images/logo.png" alt="" className="h-7" />
+        </a>
+      </div>
+      <div id='contact' className="flex max-lg:flex-col mt-[140px] lg:items-center py-12 md:py-24 gap-20">
+        <div className="flex-1">
+          <h1 className="font-semibold text-6xl leading-tight text-slate-800 mt-10">Contact Us</h1>
+          <p className="mt-2 text-black text-xl">April 2023 - Karachi</p>
+          <p className="mt-4 text-lg">
+          For the Next Generation and Future of the Internet <br></br> Join a 13 Trillion Dollar Industry with 5 Billion Users
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+          </p>
+        </div>
+        <div className="flex-1">
+          <div className="rounded-lg bg-white shadow-xl max-w-lg">
+            <div className="grid place-items-center rounded-t-lg bg-black p-8 text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-12 w-12">
+                <path
+                  d="M19.5 22.5a3 3 0 003-3v-8.174l-6.879 4.022 3.485 1.876a.75.75 0 01-.712 1.321l-5.683-3.06a1.5 1.5 0 00-1.422 0l-5.683 3.06a.75.75 0 01-.712-1.32l3.485-1.877L1.5 11.326V19.5a3 3 0 003 3h15z"
+                />
+                <path
+                  d="M1.5 9.589v-.745a3 3 0 011.578-2.641l7.5-4.039a3 3 0 012.844 0l7.5 4.039A3 3 0 0122.5 8.844v.745l-8.426 4.926-.652-.35a3 3 0 00-2.844 0l-.652.35L1.5 9.59z"
+                />
+              </svg>
+            </div>
+            <div className="p-10">
+              <p>Get notified of all the updates</p>
+              <form action="" className="mt-3 flex max-sm:flex-col max-sm:gap-3">
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  className="rounded-md border sm:rounded-r-none border-gray-300 px-4 py-2 flex-grow hover:border-black focus:border-black focus:outline-none"
+                />
+                <button className="rounded-md sm:rounded-l-none bg-black px-4 py-2 font-semibold text-white">
+                  Notify me
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        <div className='logos flex justify-center'>
+<a className=' p-2' href="https://www.piaic.org/" target="_blank">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-12 h-12">
+  <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
+  <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
+  <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
+</svg>
+</a>
+<a className=' p-3' href='https://www.facebook.com/groups/panaverse' target={'_blank'}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-12 w-12" fill="currentColor" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+  </svg>
+  </a>
+  <a  className=' p-3' href='https://www.youtube.com/@panaverse/streams' target={'_blank'}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-12 w-12"
+    fill="currentColor"
+    viewBox="0 0 24 24">
+    <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+  </svg>
+  </a>
+  <a className=' p-3' href='https://twitter.com/Panaverse_edu' target={'_blank'}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-12 w-12"
+    fill="currentColor"
+    viewBox="0 0 24 24">
+    <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+  </svg>
+  </a>
+  <a className=' p-3' href='https://github.com/panaverse' target={'_blank'}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-12 w-12"
+    fill="currentColor"
+    viewBox="0 0 24 24">
+    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+  </svg>
+  </a>
+  <h1 className=' font-semibold text-slate-900 text-2xl p-1 ml-36'>
+          This website made by:<br></br>
+          <a href='https://github.com/MuhammadMuzammilKhan951' target='_blank'>Muhammad Muzammil Khan</a>
+        </h1>
       </div>
-    </main>
+    </div>
   )
 }
